1fcb8d29057f2d13954749419ee8243fb6289d18
Unknown <matusf.ttmodely@gmail.com>
2017-06-17 15:51:08 +0200

    #1-danger-dog-circle

--- maps.py
+++ maps.py
@@ -1,13 +1,11 @@
-from folium import Map, FeatureGroup, Marker, Icon, RegularPolygonMarker, GeoJson, LayerControl
-from geopy.geocoders import Nominatim
+from folium import Map, FeatureGroup, Marker, Icon, RegularPolygonMarker, GeoJson, LayerControl, CircleMarker
 from json import loads
-# from threading import Thread
 import codecs
 import requests
 
 
 def get_long_lat(address):
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}'.format(
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}&key=AIzaSyAZdptvPWWMO060tQpmbN_GKCZFjcCZzEY'.format(
         address.replace(' ', '+'))
     response = requests.get(url)
     payload = response.json()
@@ -24,8 +22,13 @@ def mark_on_map(dog, dogos):
     if not (dog.get('-col_6', False) and dog.get('-col_5', False) and dog.get('-col_4', False)):
         return
     dog_location = get_long_lat('{-col_6} {-col_5}, {-col_4}'.format(**dog))
-    dogos.add_child(RegularPolygonMarker(location=dog_location,
-                                         number_of_sides=8, radius=10))
+    print('{-col_6} {-col_5}, {-col_4}'.format(**dog))
+    print(dog_location)
+    dogos.add_child(RegularPolygonMarker(location=dog_location, fill_color='red' if dog[
+                    '-col_3'] == 'Ano' else 'blue', popup=dog['-col_0'], number_of_sides=8, radius=10))
+    # If dogo is aggressive
+    if dog['-col_3'] == 'Ano':
+        dogos.add_child(CircleMarker(location=dog_location, radius=25, fill_color='#A43747'))
 
 
 map = Map(location=get_long_lat('Presov'), zoom_start=13)
@@ -38,6 +41,7 @@ for dog in dogos_file['ds']['rs']['data']['row']:
     if c > 10:
         break
     c += 1
+    # print(c)
     mark_on_map(dog, dogos)
 
 map.add_child(dogos)
